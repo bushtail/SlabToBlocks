41a9290dc2dad28f990d46333797fb30845e205b
Evan Nosich <evansaccounts@outlook.com>
2019-08-29 15:09:41 -0400

    Removed unused imports.

--- src/ca/evannosich/slabtoblocks/SlabToBlocks.java
+++ src/ca/evannosich/slabtoblocks/SlabToBlocks.java
@@ -4,9 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SlabToBlocks extends JavaPlugin {
