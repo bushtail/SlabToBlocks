313db10da1ff1290e6f2d6138808fab4dbc8c55e
Evan Nosich <evansaccounts@outlook.com>
2019-08-29 17:04:57 -0400

    Added Overworld Materials - Land

--- src/ca/evannosich/slabtoblocks/SlabToBlocks.java
+++ src/ca/evannosich/slabtoblocks/SlabToBlocks.java
@@ -11,65 +11,249 @@ public class SlabToBlocks extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		// Oak Planks
+		// Planks
 		
-		ItemStack oakPlanksItem = new ItemStack(Material.OAK_PLANKS);
-		
-		NamespacedKey oakPlanksKey = new NamespacedKey(this, "OAK_PLANKS");
-		
-		ShapelessRecipe oakPlanksRecipe = new ShapelessRecipe(oakPlanksKey, oakPlanksItem);
-		
-		oakPlanksRecipe.addIngredient(2, Material.OAK_SLAB);
-		
-		// Spruce Planks
-		
-		ItemStack sprucePlanksItem = new ItemStack(Material.SPRUCE_PLANKS);
-		
-		NamespacedKey sprucePlanksKey = new NamespacedKey(this, "SPRUCE_PLANKS");
-		
-		ShapelessRecipe sprucePlanksRecipe = new ShapelessRecipe(sprucePlanksKey, sprucePlanksItem);
-		
-		sprucePlanksRecipe.addIngredient(2, Material.SPRUCE_SLAB);
-		
-		// Birch Planks
-		
-		ItemStack birchPlanksItem = new ItemStack(Material.BIRCH_PLANKS);
-		
-		NamespacedKey birchPlanksKey = new NamespacedKey(this, "BIRCH_PLANKS");
-		
-		ShapelessRecipe birchPlanksRecipe = new ShapelessRecipe(birchPlanksKey, birchPlanksItem);
-		
-		birchPlanksRecipe.addIngredient(2, Material.BIRCH_SLAB);
-		
-		// Jungle Planks
-		
-		ItemStack junglePlanksItem = new ItemStack(Material.JUNGLE_PLANKS);
-		
-		NamespacedKey junglePlanksKey = new NamespacedKey(this, "JUNGLE_PLANKS");
-		
-		ShapelessRecipe junglePlanksRecipe = new ShapelessRecipe(junglePlanksKey, junglePlanksItem);
-		
-		junglePlanksRecipe.addIngredient(2, Material.JUNGLE_SLAB);
-		
-		// Acacia Planks
-		
-		ItemStack acaciaPlanksItem = new ItemStack(Material.ACACIA_PLANKS);
-		
-		NamespacedKey acaciaPlanksKey = new NamespacedKey(this, "ACACIA_PLANKS");
-		
-		ShapelessRecipe acaciaPlanksRecipe = new ShapelessRecipe(acaciaPlanksKey, acaciaPlanksItem);
-		
-		acaciaPlanksRecipe.addIngredient(2, Material.ACACIA_SLAB);
-		
-		// Dark Oak Planks
-		
-		ItemStack darkOakPlanksItem = new ItemStack(Material.DARK_OAK_PLANKS);
-		
-		NamespacedKey darkOakPlanksKey = new NamespacedKey(this, "DARK_OAK_PLANKS");
-		
-		ShapelessRecipe darkOakPlanksRecipe = new ShapelessRecipe(darkOakPlanksKey, darkOakPlanksItem);
-		
-		darkOakPlanksRecipe.addIngredient(2, Material.DARK_OAK_SLAB);
+			// Oak Planks
+			
+			ItemStack oakPlanksItem = new ItemStack(Material.OAK_PLANKS);
+			
+			NamespacedKey oakPlanksKey = new NamespacedKey(this, "OAK_PLANKS");
+			
+			ShapelessRecipe oakPlanksRecipe = new ShapelessRecipe(oakPlanksKey, oakPlanksItem);
+			
+			oakPlanksRecipe.addIngredient(2, Material.OAK_SLAB);
+			
+			// Spruce Planks
+			
+			ItemStack sprucePlanksItem = new ItemStack(Material.SPRUCE_PLANKS);
+			
+			NamespacedKey sprucePlanksKey = new NamespacedKey(this, "SPRUCE_PLANKS");
+			
+			ShapelessRecipe sprucePlanksRecipe = new ShapelessRecipe(sprucePlanksKey, sprucePlanksItem);
+			
+			sprucePlanksRecipe.addIngredient(2, Material.SPRUCE_SLAB);
+			
+			// Birch Planks
+			
+			ItemStack birchPlanksItem = new ItemStack(Material.BIRCH_PLANKS);
+			
+			NamespacedKey birchPlanksKey = new NamespacedKey(this, "BIRCH_PLANKS");
+			
+			ShapelessRecipe birchPlanksRecipe = new ShapelessRecipe(birchPlanksKey, birchPlanksItem);
+			
+			birchPlanksRecipe.addIngredient(2, Material.BIRCH_SLAB);
+			
+			// Jungle Planks
+			
+			ItemStack junglePlanksItem = new ItemStack(Material.JUNGLE_PLANKS);
+			
+			NamespacedKey junglePlanksKey = new NamespacedKey(this, "JUNGLE_PLANKS");
+			
+			ShapelessRecipe junglePlanksRecipe = new ShapelessRecipe(junglePlanksKey, junglePlanksItem);
+			
+			junglePlanksRecipe.addIngredient(2, Material.JUNGLE_SLAB);
+			
+			// Acacia Planks
+			
+			ItemStack acaciaPlanksItem = new ItemStack(Material.ACACIA_PLANKS);
+			
+			NamespacedKey acaciaPlanksKey = new NamespacedKey(this, "ACACIA_PLANKS");
+			
+			ShapelessRecipe acaciaPlanksRecipe = new ShapelessRecipe(acaciaPlanksKey, acaciaPlanksItem);
+			
+			acaciaPlanksRecipe.addIngredient(2, Material.ACACIA_SLAB);
+			
+			// Dark Oak Planks
+			
+			ItemStack darkOakPlanksItem = new ItemStack(Material.DARK_OAK_PLANKS);
+			
+			NamespacedKey darkOakPlanksKey = new NamespacedKey(this, "DARK_OAK_PLANKS");
+			
+			ShapelessRecipe darkOakPlanksRecipe = new ShapelessRecipe(darkOakPlanksKey, darkOakPlanksItem);
+			
+			darkOakPlanksRecipe.addIngredient(2, Material.DARK_OAK_SLAB);
+			
+		// Overworld Materials - Land
+			
+			// Cobblestone
+			
+			ItemStack cobblestoneItem = new ItemStack(Material.COBBLESTONE);
+			
+			NamespacedKey cobblestoneKey = new NamespacedKey(this, "COBBLESTONE");
+			
+			ShapelessRecipe cobblestoneRecipe = new ShapelessRecipe(cobblestoneKey, cobblestoneItem);
+			
+			cobblestoneRecipe.addIngredient(2, Material.COBBLESTONE_SLAB);
+			
+			// Stone
+			
+			ItemStack stoneItem = new ItemStack(Material.STONE);
+			
+			NamespacedKey stoneKey = new NamespacedKey(this, "STONE");
+			
+			ShapelessRecipe stoneRecipe = new ShapelessRecipe(stoneKey, stoneItem);
+			
+			stoneRecipe.addIngredient(2, Material.STONE_SLAB);
+			
+			// Smooth Stone
+			
+			ItemStack smoothStoneItem = new ItemStack(Material.SMOOTH_STONE);
+			
+			NamespacedKey smoothStoneKey = new NamespacedKey(this, "SMOOTH_STONE");
+			
+			ShapelessRecipe smoothStoneRecipe = new ShapelessRecipe(smoothStoneKey, smoothStoneItem);
+			
+			smoothStoneRecipe.addIngredient(2, Material.SMOOTH_STONE_SLAB);
+			
+			// Bricks
+			
+			ItemStack bricksItem = new ItemStack(Material.BRICKS);
+			
+			NamespacedKey bricksKey = new NamespacedKey(this, "BRICKS");
+			
+			ShapelessRecipe bricksRecipe = new ShapelessRecipe(bricksKey, bricksItem);
+			
+			bricksRecipe.addIngredient(2, Material.BRICK_SLAB);
+			
+			// Andesite
+			
+			ItemStack andesiteItem = new ItemStack(Material.ANDESITE);
+			
+			NamespacedKey andesiteKey = new NamespacedKey(this, "ANDESITE");
+			
+			ShapelessRecipe andesiteRecipe = new ShapelessRecipe(andesiteKey, andesiteItem);
+			
+			andesiteRecipe.addIngredient(2, Material.ANDESITE_SLAB);
+			
+			// Polished Andesite
+			
+			ItemStack polishedAndesiteItem = new ItemStack(Material.POLISHED_ANDESITE);
+			
+			NamespacedKey polishedAndesiteKey = new NamespacedKey(this, "POLISHED_ANDESITE");
+			
+			ShapelessRecipe polishedAndesiteRecipe = new ShapelessRecipe(polishedAndesiteKey, polishedAndesiteItem);
+			
+			polishedAndesiteRecipe.addIngredient(2, Material.POLISHED_ANDESITE_SLAB);
+			
+			// Diorite
+			
+			ItemStack dioriteItem = new ItemStack(Material.DIORITE);
+			
+			NamespacedKey dioriteKey = new NamespacedKey(this, "DIORITE");
+			
+			ShapelessRecipe dioriteRecipe = new ShapelessRecipe(dioriteKey, dioriteItem);
+			
+			dioriteRecipe.addIngredient(2, Material.DIORITE_SLAB);
+			
+			// Polished Diorite
+			
+			ItemStack polishedDioriteItem = new ItemStack(Material.POLISHED_DIORITE);
+			
+			NamespacedKey polishedDioriteKey = new NamespacedKey(this, "POLISHED_DIORITE");
+			
+			ShapelessRecipe polishedDioriteRecipe = new ShapelessRecipe(polishedDioriteKey, polishedDioriteItem);
+			
+			polishedDioriteRecipe.addIngredient(2, Material.POLISHED_DIORITE_SLAB);
+			
+			// Granite
+			
+			ItemStack graniteItem = new ItemStack(Material.GRANITE);
+			
+			NamespacedKey graniteKey = new NamespacedKey(this, "GRANITE");
+			
+			ShapelessRecipe graniteRecipe = new ShapelessRecipe(graniteKey, graniteItem);
+			
+			graniteRecipe.addIngredient(2, Material.GRANITE_SLAB);
+			
+			// Polished Granite
+			
+			ItemStack polishedGraniteItem = new ItemStack(Material.POLISHED_GRANITE);
+			
+			NamespacedKey polishedGraniteKey = new NamespacedKey(this, "POLISHED_GRANITE");
+			
+			ShapelessRecipe polishedGraniteRecipe = new ShapelessRecipe(polishedGraniteKey, polishedGraniteItem);
+			
+			polishedGraniteRecipe.addIngredient(2, Material.POLISHED_GRANITE_SLAB);
+			
+			// Mossy Stone Bricks
+			
+			ItemStack mossyStoneBricksItem = new ItemStack(Material.MOSSY_STONE_BRICKS);
+			
+			NamespacedKey mossyStoneBricksKey = new NamespacedKey(this, "MOSSY_STONE_BRICKS");
+			
+			ShapelessRecipe mossyStoneBricksRecipe = new ShapelessRecipe(mossyStoneBricksKey, mossyStoneBricksItem);
+			
+			mossyStoneBricksRecipe.addIngredient(2, Material.MOSSY_STONE_BRICK_SLAB);
+			
+			// Mossy Cobblestone
+			
+			ItemStack mossyCobblestoneItem = new ItemStack(Material.MOSSY_COBBLESTONE);
+			
+			NamespacedKey mossyCobblestoneKey = new NamespacedKey(this, "MOSSY_COBBLESTONE");
+			
+			ShapelessRecipe mossyCobblestoneRecipe = new ShapelessRecipe(mossyCobblestoneKey, mossyCobblestoneItem);
+			
+			mossyCobblestoneRecipe.addIngredient(2, Material.MOSSY_COBBLESTONE_SLAB);
+			
+			// Smooth Sandstone
+			
+			ItemStack smoothSandstoneItem = new ItemStack(Material.SMOOTH_SANDSTONE);
+			
+			NamespacedKey smoothSandstoneKey = new NamespacedKey(this, "SMOOTH_SANDSTONE");
+			
+			ShapelessRecipe smoothSandstoneRecipe = new ShapelessRecipe(smoothSandstoneKey, smoothSandstoneItem);
+			
+			smoothSandstoneRecipe.addIngredient(2, Material.SMOOTH_SANDSTONE_SLAB);
+			
+			// Smooth Red Sandstone
+			
+			ItemStack smoothRedSandstoneItem = new ItemStack(Material.SMOOTH_RED_SANDSTONE);
+			
+			NamespacedKey smoothRedSandstoneKey = new NamespacedKey(this, "SMOOTH_RED_SANDSTONE");
+			
+			ShapelessRecipe smoothRedSandstoneRecipe = new ShapelessRecipe(smoothRedSandstoneKey, smoothRedSandstoneItem);
+			
+			smoothRedSandstoneRecipe.addIngredient(2, Material.SMOOTH_RED_SANDSTONE_SLAB);
+			
+			// Cut Sandstone
+			
+			ItemStack cutSandstoneItem = new ItemStack(Material.CUT_SANDSTONE);
+			
+			NamespacedKey cutSandstoneKey = new NamespacedKey(this, "CUT_SANDSTONE");
+			
+			ShapelessRecipe cutSandstoneRecipe = new ShapelessRecipe(cutSandstoneKey, cutSandstoneItem);
+			
+			cutSandstoneRecipe.addIngredient(2, Material.CUT_SANDSTONE_SLAB);
+			
+			// Cut Red Sandstone
+			
+			ItemStack cutRedSandstoneItem = new ItemStack(Material.CUT_RED_SANDSTONE);
+			
+			NamespacedKey cutRedSandstoneKey = new NamespacedKey(this, "CUT_RED_SANDSTONE");
+			
+			ShapelessRecipe cutRedSandstoneRecipe = new ShapelessRecipe(cutRedSandstoneKey, cutRedSandstoneItem);
+			
+			cutRedSandstoneRecipe.addIngredient(2, Material.CUT_RED_SANDSTONE_SLAB);
+			
+		// Overworld Materials - Underwater
+			
+			// Prismarine
+			
+			// Prismarine Brick
+			
+			// Dark Prismarine
+			
+		// Nether Materials
+			
+			// Nether Brick
+			
+			// Red Nether Brick
+			
+			// Smooth Quartz
+			
+		// End Materials
+			
+			// End Stone Brick
 		
 		// Add Recipes
 		Bukkit.addRecipe(oakPlanksRecipe);
@@ -78,6 +262,19 @@ public class SlabToBlocks extends JavaPlugin {
 		Bukkit.addRecipe(junglePlanksRecipe);
 		Bukkit.addRecipe(acaciaPlanksRecipe);
 		Bukkit.addRecipe(darkOakPlanksRecipe);
+		Bukkit.addRecipe(cobblestoneRecipe);
+		Bukkit.addRecipe(stoneRecipe);
+		Bukkit.addRecipe(smoothStoneRecipe);
+		Bukkit.addRecipe(bricksRecipe);
+		Bukkit.addRecipe(andesiteRecipe);
+		Bukkit.addRecipe(polishedAndesiteRecipe);
+		Bukkit.addRecipe(dioriteRecipe);
+		Bukkit.addRecipe(polishedDioriteRecipe);
+		Bukkit.addRecipe(graniteRecipe);
+		Bukkit.addRecipe(polishedGraniteRecipe);
+		Bukkit.addRecipe(mossyStoneBricksRecipe);
+		Bukkit.addRecipe(smoothSandstoneRecipe);
+		Bukkit.addRecipe(smoothRedSandstoneRecipe);
 	}
 	
 	@Override
